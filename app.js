18a2db8eb9ba7722117cb44d83ce4f30af9b097a
sardor2528 <abdulkhodievsardor@gmail.com>
2023-08-01 13:26:47 +0500

    realme

---
+++ app.js
@@ -0,0 +1,114 @@
+// let ism = prompt("your name")
+// // console.log(ism);
+
+// // console.log("Hello " + ism);
+
+// let yil = prompt("tugilgann yiliz")
+// // console.log(2023 - yil);
+
+// console.log("men " + ism + " " + (2023 - yil) + " yoshman");
+
+// console.log(`men ${ism} ${2023 - yil} yoshman`);
+
+// let a = prompt("a"),
+//     b = prompt("b")
+// console.log(`${a} + ${b} = ${+a + +b}`);
+
+
+// let a = 5
+// a = 15
+// console.log(a);
+
+
+// const b = 3
+// // b = 45 // error
+// console.log(b);
+
+
+// let num = 15
+// let big = 15n
+// console.log(typeof num);
+// console.log(typeof big);
+
+// let s = Symbol(15)
+// let s1 = Symbol("salom")
+// console.log(typeof s);
+
+
+//Reference -> Object
+//1.Array
+//2.Object
+
+//array - index and value
+// let fruits = ["apple", "banana", "ananas", 25]
+// console.log(fruits);
+// console.log(fruits[1]);
+// console.log(fruits[3] * 2);
+
+// //object - key and value
+// let user = {
+//     name: "sardor",
+//     username: "abdulkhodiev",
+//     password: 123456789
+// }
+
+// console.log(user);
+// console.log(user.password);
+
+// let arr = [20, "salom", true, null, [25, 30], { name: "John" },
+//     {
+//         ism: "Mike",
+//         address: {
+//             country: "Usa",
+//             city: "New York"
+//         }
+//     },
+//     {
+//         a: 15,
+//         b: [
+//             true,
+//             {
+//                 c: "mana shu chiqishi kerak"
+//             }
+//         ]
+//     }
+// ]
+// console.log(arr);
+// console.log(arr[4][1]);
+// console.log(arr[5].name);
+// console.log(arr[7].b[1].c);
+
+
+// let instaBase = [
+//     {
+//         name: "Boburmirzo",
+//         username: "boburmirzo_brm",
+//         password: "********",
+//         bio: "Trust but Verify",
+//         avatar: "https://",
+//         following: ["id56156165", "id151231313"],
+//         followers: ["id56156165", "id151231313", "id151231313", "id-selena"],
+//         posts: [
+//             {
+//                 url: "https",
+//                 likes: ["id1231321", "id1561561"],
+//                 comments: [
+//                     {
+//                         name: "Sardor",
+//                         msg: "ustoz codelarni tawlavori"
+//                     },
+//                     {
+//                         name: "Muhammadsodiq",
+//                         msg: "Bugun darsmi"
+//                     },
+//                 ]
+//             }
+//         ]
+//     }
+// ]
+// console.log(instaBase);
+// console.log(instaBase[0].posts[0].comments[1].msg);
+
+
+
+
